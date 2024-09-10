ce6f97131b95fa35f3d3cc7019584ff6fe9d94f9
Willyam <willyam.mgomes@gmail.com>
2024-09-10 12:47:53 -0300

    cadastrar metas

---
+++ index.js
@@ -0,0 +1,58 @@
+const { select, input } = require('@inquirer/prompts')
+
+let meta = {
+    value: "Tomar 3L de água por dia",
+    checked: false
+}
+let metas = [ meta ]
+
+const cadastrarMeta = async () => {
+    const meta = await input({ message: "Digite a meta:"})
+
+    if(meta.length == 0){
+        console.log("A meta não pode ser vazia")
+        return 
+    }
+
+    metas.push({
+        value: meta, checked: false
+    })
+}
+
+const start = async () => {
+  
+    while(true){
+        const opcao = await select({
+            message: "Menu >",
+            choices: [
+                {
+                name: "Cadastrar meta",
+                value:"cadastrar"
+            },
+            {
+                name: "Listar metas",
+                value: "listar"
+            },
+            {
+                name: "Sair",
+                value: "sair"
+            }
+        
+        ]
+        })
+        switch(opcao){
+            case "cadastrar":
+                await cadastrarMeta()
+                console.log(metas)
+                break
+            case "listar":
+                console.log("vamos listar")
+                break
+            case "sair":
+                console.log("Até a próxima")
+                return       
+        }
+   }
+}
+
+start()
