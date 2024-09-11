befaeb9df9c82e85fa6fdbe9c3fd356f2ac06985
Willyam <willyam.mgomes@gmail.com>
2024-09-11 10:59:24 -0300

    resolvendo bug listarMetas

--- index.js
+++ index.js
@@ -27,15 +27,17 @@ const listarMetas = async () => {
 
     })
 
+    metas.forEach((m) => {
+        m.checked = false
+    })
+
+
     if(respostas.length == 0){
         console.log("Nenhuma meta selecionada!")
         return
     }
 
-    metas.forEach((m) => {
-        m.checked = false
-    })
-
+    
     respostas.forEach((resposta) => {
         const meta = metas.find((m) => {
             return m.value == resposta
